3325f8db124be2fd357856dbc5bf59abb7d8c277
Jessica Sandoval <jsandoval@rtvc.gov.co>
2020-12-03 20:47:13 -0500

    fixed len

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,7 +6,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices"""
     if axis == 1 and (len(mat1) == len(mat2)):
         return [mat1[index]+mat2[index] for index, item in enumerate(mat2)]
-    if axis == 0:
+    if axis == 0 and (len(mat1[0]) == len(mat2[0])):
         row1 = [arr[:] for arr in mat1]
         row2 = [arr2[:] for arr2 in mat2]
         return row1 + row2
