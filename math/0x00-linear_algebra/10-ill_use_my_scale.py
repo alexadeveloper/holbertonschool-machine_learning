b8a567120bac62faad80d328aa5cb6b0771917cc
Jessica Sandoval <jsandoval@rtvc.gov.co>
2020-12-03 21:42:24 -0500

    fixed import numpy

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -2,10 +2,6 @@
 """ function matrix shape """
 
 
-import numpy as np
-
-
 def np_shape(matrix):
     """ matrix shape function"""
-    mat = np.array(matrix)
-    return mat.shape
+    return matrix.shape
