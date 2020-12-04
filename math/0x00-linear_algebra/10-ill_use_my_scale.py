6662938417920fd992e0a4639cc33065e42499de
Jessica Sandoval <jsandoval@rtvc.gov.co>
2020-12-03 21:39:47 -0500

    task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function matrix shape """
+
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """ matrix shape function"""
+    mat = np.array(matrix)
+    return mat.shape
