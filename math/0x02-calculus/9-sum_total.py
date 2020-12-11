86337634168b83317f458b2736332c5831633796
Jessica Sandoval <jsandoval@rtvc.gov.co>
2020-12-10 22:55:57 -0500

    fixed not loop

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,9 +4,8 @@
 
 def summation_i_squared(n):
     """ function summation """
-    i = 1
-    suma = 0
-    while(i <= n):
-        suma = suma + (i * i)
-        i = i + 1
-    return suma
+    if n <= 0:
+        return None
+    if type(n) != int:
+        return None
+    return int((n * (1 + n) * (1 + 2 * n)) / 6)
