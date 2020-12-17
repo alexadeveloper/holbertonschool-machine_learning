36072d5423d84dcdcb9cf7aecb9ea891936ddf3d
Jessica Sandoval <jsandoval@rtvc.gov.co>
2020-12-16 20:26:44 -0500

    task 3 the class

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -2,15 +2,18 @@
 """ exponential """
 
 
-def __init__(self, data=None, lambtha=1.):
-    self.lambtha = float(lambtha)
-    if data is None:
-        if lambtha <= 0:
-            raise ValueError("lambtha must be a positive value")
-    else:
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
-        isList = type(data)
-        if isList != list:
-            raise TypeError("data must be a list")
-        self.lambtha = float(sum(data) / len(data))
+class Exponential:
+    """ exponential"""
+    def __init__(self, data=None, lambtha=1.):
+        """ function lambtha"""
+        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            isList = type(data)
+            if isList != list:
+                raise TypeError("data must be a list")
+            self.lambtha = float(sum(data) / len(data))
